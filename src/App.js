ddce61ec0d7e0e6bacadacdc7a2f2055aa6d7df5
DiegoSullon <67489081+DiegoSullon@users.noreply.github.com>
2021-06-30 13:58:51 -0500

    delete css-loader and style-loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import './styles/styles.css'
 import { Navbar } from './components/Navbar/Navbar.js'
 import { Main } from './components/Main/Main'
 import { Controls } from './components/Controls/Controls'
